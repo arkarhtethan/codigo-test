2a2eeed5800030c668e2a65e6bddcb77dd634711
Kaung Myat Han <arkarhtethan@gmail.com>
2022-01-10 21:34:48 +0630

    Work container added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Footer from './components/Footer';
 import NavBar from './components/NavBar';
+import Work from './containers/Work';
 
 function App () {
   return (
     <div className="App">
       <NavBar />
+      <Work />
       <Footer />
     </div>
   );
