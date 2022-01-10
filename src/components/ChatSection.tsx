19038eb383cc63df13faa9cbe1771efc74657a12
Kaung Myat Han <arkarhtethan@gmail.com>
2022-01-10 19:31:14 +0630

    Footer section finished

---
+++ src/components/ChatSection.tsx
@@ -0,0 +1,41 @@
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+
+interface IChatItemProps {
+    title: string;
+    description: string;
+}
+const ChatSection = () => {
+    const ChatItem = ({ description, title }: IChatItemProps) => {
+        return <div className="cursor-pointer">
+            <div className="flex space-x-4 items-center">
+                <p className="font-bold text-2xl mb-2">
+                    {title}
+                </p>
+                <FontAwesomeIcon icon={faArrowRight} className="text-sm mb-3 primary-color" />
+            </div>
+            <p className="text-sm">{description}</p>
+        </div>
+    }
+
+    const VerticalSeparator = () => <div className="border-r-2 border-gray-200 pr-2">
+
+    </div>
+
+    return (
+        <div className="my-16">
+            <h1 className="primary-color font-bold text-4xl text-center mb-12">Let's have a chat</h1>
+            <div className="flex space-x-12">
+                <ChatItem title="Build" description="Help you build something" />
+                <VerticalSeparator />
+                <ChatItem title="Co-incubate" description="Co-incubate an idea together" />
+                <VerticalSeparator />
+                <ChatItem title="Customise" description="Customise a solution for your business" />
+                <VerticalSeparator />
+                <ChatItem title="Organise" description="Organise learning sessions with us" />
+            </div>
+        </div>
+    )
+}
+
+export default ChatSection
---
+++ src/components/Footer.tsx
@@ -0,0 +1,33 @@
+import { faFacebookF, faInstagram, faLinkedinIn, faTwitter } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React from 'react'
+import ChatSection from './ChatSection'
+
+const Footer = () => {
+    return (
+        <div className="bg-white w-full flex flex-col items-center space-y-5">
+            <ChatSection />
+            {/* icon */}
+            <div className="flex text-lg space-x-8">
+                <a href="https://www.facebook.com/codigo.co/" target="_blank" rel="noreferrer">
+                    <FontAwesomeIcon icon={faFacebookF} />
+                </a>
+                <a href="https://twitter.com/CodigoApps" target="_blank" rel="noreferrer">
+                    <FontAwesomeIcon icon={faTwitter} />
+                </a>
+                <a href="https://www.instagram.com/hellocodigo/" target="_blank" rel="noreferrer">
+                    <FontAwesomeIcon icon={faInstagram} />
+                </a>
+                <a href="https://www.linkedin.com/company/codigo-pte-ltd" target="_blank" rel="noreferrer">
+                    <FontAwesomeIcon icon={faLinkedinIn} />
+                </a>
+            </div>
+            <div className="flex flex-col items-center text-sm space-y-3">
+                <p>Copyright © Codigo - Mobile App Developer Singapore</p>
+                <p>+65 6455 9790 • 24 Sin Ming Lane, Midview City #04-91 Singapore 573970</p>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
