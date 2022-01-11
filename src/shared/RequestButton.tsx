d1b560fb8f12335f5e5770296235a6d25de80b25
Kaung Myat Han <arkarhtethan@gmail.com>
2022-01-11 10:08:43 +0630

    request button extracted

---
+++ src/shared/RequestButton.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RequestButton = () => {
+    return (
+        <div className="bg-gray-800 text-white rounded-full px-4 py-4">Request a quote</div>
+    )
+}
+
+export default RequestButton;
