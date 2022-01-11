12237b00893aca8ea61738e36d05502db9e435d6
Kaung Myat Han <arkarhtethan@gmail.com>
2022-01-11 10:06:28 +0630

    react router added

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import Footer from './components/Footer';
 import NavBar from './components/NavBar';
+import ProjectDetails from './containers/ProjectDetails';
 import Work from './containers/Work';
 
 function App () {
   return (
     <div className="App">
       <NavBar />
-      <Work />
+      <Routes>
+        <Route path="/" element={<Work />} />
+        <Route path="/:id" element={<ProjectDetails />} />
+      </Routes>
       <Footer />
-    </div>
+    </div >
   );
 }
 
